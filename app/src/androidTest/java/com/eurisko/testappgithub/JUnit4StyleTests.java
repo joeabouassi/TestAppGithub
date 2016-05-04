fc38665c923ec9d2f1ad58becb4e966ccc258a5f
joeabouassi <joe.aa93@gmail.com>
2016-05-04 14:32:56 +0300

    added Espresso AndroidTest and ScreenGrab dependency

---
+++ app/src/androidTest/java/com/eurisko/testappgithub/JUnit4StyleTests.java
@@ -0,0 +1,33 @@
+package com.eurisko.testappgithub;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(JUnit4.class)
+public class JUnit4StyleTests {
+    @ClassRule
+    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testTakeMoreScreenshots() {
+        Screengrab.screenshot("mainActivity");
+
+        onView(withId(R.id.hello)).check(matches(isDisplayed()));
+    }
+}
