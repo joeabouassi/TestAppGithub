412fa028ff14df0444ce29d921e0fdbbf5c29818
joeabouassi <joe.aa93@gmail.com>
2016-05-06 15:01:56 +0300

    added unity project

---
+++ UBA/Assets/Editor/AutoBuilder.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+
+public static class AutoBuilder
+{
+
+    private static string GetProjectName()
+    {
+        string[] s = Application.dataPath.Split('/');
+        string projectName = s[s.Length - 2];
+        Debug.Log("project Name: " + projectName);
+        return projectName;
+    }
+
+    [MenuItem("AutoBuilder/Build Android")]
+    static void PerformAndroidBuild()
+    {
+
+        string formattedTimeStamp =
+            System.DateTime.Now.Day.ToString()      + "-" +
+            System.DateTime.Now.Month.ToString()    + "-" +
+            System.DateTime.Now.Year.ToString()     + "[" +
+            System.DateTime.Now.Hour.ToString()     + ":" +
+            System.DateTime.Now.Minute.ToString()   + "]";
+
+        Debug.Log(formattedTimeStamp);
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
+        BuildPipeline.BuildPlayer(
+
+            new string [] {"Assets/Scenes/MainScene.unity"}, 
+            "Builds/Android/" + GetProjectName() + "-" + formattedTimeStamp + ".apk", 
+            BuildTarget.Android, 
+            BuildOptions.None
+
+        );
+
+    }
+
+}
---
+++ UBA/Assets/Scripts/oscillate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class oscillate : MonoBehaviour {
+
+    public float randOffset;
+    public float Increment;
+
+    private float offset;
+    private float r;
+
+	// Use this for initialization
+	void Start () {
+
+        offset = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        r = Random.Range(0, randOffset);
+
+        offset += Increment + Mathf.Tan(r);
+
+        Vector3 yPos = new Vector3(this.transform.position.x, Mathf.Sin(offset), this.transform.position.z);
+        this.transform.position = yPos;
+
+	}
+}
